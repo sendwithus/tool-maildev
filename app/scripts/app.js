eb6da99551cf267ca46cbcb12c9080ec4f1c3951
Brandon Brown <brandon@brandonb.io>
2015-08-22 19:55:36 -0700

    Add HTML5 routing with pushstate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,75 +4,85 @@
  * App Config
  */
 
-var app = angular.module('mailDevApp', ['ngRoute', 'ngResource',  'ngSanitize']);
+var app = angular.module('mailDevApp', ['ngRoute', 'ngResource', 'ngSanitize'])
 
-app.config(['$routeProvider', function($routeProvider){
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
   $routeProvider
-    .when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl' })
-    .when('/email/:itemId', { templateUrl: 'views/item.html', controller: 'ItemCtrl' })
-    .otherwise({ redirectTo: '/' });
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/email/:itemId', {
+      templateUrl: 'views/item.html',
+      controller: 'ItemCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    })
 
-}]);
+  $locationProvider.html5Mode(true)
 
-app.run(['$rootScope', function($rootScope){
-  
+}])
+
+app.run(['$rootScope', function ($rootScope) {
+
   // Connect Socket.io
-  var socket = io();
+  var socket = io()
 
-  socket.on('newMail', function(data) {
-    $rootScope.$emit('newMail', data);
-  });
-
-  socket.on('deleteMail', function(data) {
-    $rootScope.$emit('deleteMail', data);
+  socket.on('newMail', function (data) {
+    $rootScope.$emit('newMail', data)
   })
-  
-  $rootScope.$on('Refresh', function() {
-    console.log('Refresh event called.');
-  });
 
-}]);
+  socket.on('deleteMail', function (data) {
+    $rootScope.$emit('deleteMail', data)
+  })
+
+  $rootScope.$on('Refresh', function () {
+    console.log('Refresh event called.')
+  })
+
+}])
 
 /**
  * NewLineFilter -- Converts new line characters to br tags
  */
 
-app.filter('newLines', function() {
-  
-  return function(text) {
-    return text && text.replace(/\n/g, '<br>') || '';
-  };
+app.filter('newLines', function () {
 
-});
+  return function (text) {
+    return text && text.replace(/\n/g, '<br>') || ''
+  }
+
+})
 
 /**
  * Sidebar scrollbar fixed height
  */
 
-(function(){
+;(function () {
 
-  var sidebar             = document.querySelector('.sidebar');
-  var sidebarHeader       = document.querySelector('.sidebar-header');
-  var emailList           = document.querySelector('.email-list');
-  var sidebarHeaderHeight = sidebarHeader.getBoundingClientRect().height;
-  var resizeTimeout       = null;
+  var sidebar             = document.querySelector('.sidebar')
+  var sidebarHeader       = document.querySelector('.sidebar-header')
+  var emailList           = document.querySelector('.email-list')
+  var sidebarHeaderHeight = sidebarHeader.getBoundingClientRect().height
+  var resizeTimeout       = null
 
-  function adjustEmailListHeight(){
-    var newEmailListHeight = sidebar.getBoundingClientRect().height - sidebarHeaderHeight;
-    emailList.style.height = newEmailListHeight + 'px';
+  function adjustEmailListHeight() {
+    var newEmailListHeight = sidebar.getBoundingClientRect().height - sidebarHeaderHeight
+    emailList.style.height = newEmailListHeight + 'px'
   }
 
-  adjustEmailListHeight();
+  adjustEmailListHeight()
 
-  window.onresize = function(){
-    if (resizeTimeout){
-      clearTimeout(resizeTimeout);
+  window.onresize = function () {
+    if (resizeTimeout) {
+      clearTimeout(resizeTimeout)
     }
-    resizeTimeout = window.setTimeout(function(){
-      adjustEmailListHeight();
-      resizeTimeout = null;
-    }, 300);
-  };
+    resizeTimeout = window.setTimeout(function () {
+      adjustEmailListHeight()
+      resizeTimeout = null
+    }, 300)
+  }
 
-})();
+})()
